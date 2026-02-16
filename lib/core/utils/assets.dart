b7e70264c5f1777e843e3a93efef0a93fb1854bf
Tomas-Ashraf <tomasashraf70@gmail.com>
2026-02-16 02:06:58 +0200

    create splash screen

--- lib/core/utils/assets.dart
+++ lib/core/utils/assets.dart
@@ -2,4 +2,5 @@ abstract class AssetsData {
   static const String chefLogo = 'assets/images/chef.png';
   static const String boyCharacter = 'assets/images/boy_character.png';
   static const String girlCharacter = 'assets/images/girl_character.png';
+  static const String burgerAnimation = 'assets/animation/food_animation.json';
 }
